cc74a5c411f5894d1034ee14334ce1d4f6a2e33e
Louise975 <jlayno_230000001653@uic.edu.ph>
2026-09-09 13:29:54 +0000

    4. router_config.yaml,w

--- exercise_week7.py
+++ exercise_week7.py
@@ -70,3 +70,16 @@ print(f"Loaded hostname: {loaded_config{'hostname'}}")
 print(f"Number of interfaces: {"len(loaded_config['interfaces])"}
 print()
 
+#---------------------------------------------------------------
+# EXERCISE 4: Save YAML to a file 
+#---------------------------------------------------------------
+
+print("=== EXERCISE 4: Save YAML to file ===")
+
+#FILL IN THE BLANK: open "router_config" for writing 
+with open("router_config.yaml", "w")as f:
+    yaml.dump(router_config. f, default_flow_style=False)
+
+print("saved to router_config.yaml")
+print()
+
